a51531a9093592d4736d0dd1f9ba089bdaf98770
NerdFromHell <izilovy@gmail.com>
2024-10-29 17:48:05 +0200

    updated delete sh

--- delete.sh
+++ delete.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Define variables
+REGION="us-east-1"
+CLUSTER_NAME="carmel-yoram-eks-cluster"
+
+aws eks update-kubeconfig --region "$REGION" --name "$CLUSTER_NAME"
+
 kubectl delete -f ./Helm/app-yamls/
 kubectl delete -f ./Helm/external-DNS.yaml
 kubectl delete -f ./Helm/ingressclass-resource.yaml
